062c1da2bed1424d184e7c921ba018ad6521951c
Khattana <Khattana89@gmail.com>
2014-01-25 09:44:15 -0400

    Intial example did not work, using C9

---
+++ main.js
@@ -0,0 +1,13 @@
+// code.9leap.net default template
+// based on enchant.js v0.7.1
+
+enchant();
+
+window.onload = function(){
+    var game = new Core(320, 320);
+    game.fps = 15;
+    game.onload = function(){
+        
+    };
+    game.start();
+};
