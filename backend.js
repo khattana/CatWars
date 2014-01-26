6183b86bb2a60b767cb756e808d80c4cf598e165
Siddharth <siddharth.smu@gmail.com>
2014-01-26 02:18:15 -0400

    added more functionality to buy button on menu

--- backend.js
+++ backend.js
@@ -3,8 +3,14 @@ function CalculateCost(){
 	var costObj = document.getElementById('CostCalculator');
 	var buy = costObj.cost.value;
 	//console.log(buy);
-	alert("buy: "+buy);
+	alert("buy"+buy);
 	return false;
+}
+function bullshit()
+{
+
+	document.getElementById('hey').value="yoyo";
+
 }
 
 
